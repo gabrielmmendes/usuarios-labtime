c8dda86b6e043266d702675af5046c823fe874d4
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-23 11:53:28 -0300

    -1 linha

--- src/main/java/br/com/gabrielmmendes/repository/UsuarioRepository.java
+++ src/main/java/br/com/gabrielmmendes/repository/UsuarioRepository.java
@@ -4,5 +4,4 @@ import br.com.gabrielmmendes.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
 }
