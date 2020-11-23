92ad7c7f0c5ac4750a75fc419600fe53b88dc21f
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-22 23:12:53 -0300

    Implementação classe Usuario

--- src/main/java/br/com/gabrielmmendes/model/Usuario.java
+++ src/main/java/br/com/gabrielmmendes/model/Usuario.java
@@ -3,14 +3,27 @@ package br.com.gabrielmmendes.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 public class Usuario {
-    private String name;
+
+    @Id
+    private Long codigo;
+    private String nomeCompleto;
+    private String nomeSocial;
+    private Date dataNascimento;
+    private String sexo;
     private String email;
-    private String phone;
+    private String estado;
+    private String municipio;
+    private Integer numeroAcessosCurso;
+    private String situacaoCurso;
+    private Date dataVinculo;
 }
