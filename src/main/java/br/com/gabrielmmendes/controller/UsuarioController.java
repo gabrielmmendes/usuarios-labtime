257714a4100256e8a7f87c8677e6d338aaf75564
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-23 20:17:55 -0300

    Novas funcionalidades: paginação e /all

--- src/main/java/br/com/gabrielmmendes/controller/UsuarioController.java
+++ src/main/java/br/com/gabrielmmendes/controller/UsuarioController.java
@@ -2,29 +2,26 @@ package br.com.gabrielmmendes.controller;
 
 import br.com.gabrielmmendes.model.Usuario;
 import br.com.gabrielmmendes.repository.UsuarioRepository;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
 
 @RestController
 @RequestMapping({"/usuarios"})
 public class UsuarioController {
+    @Autowired
     private UsuarioRepository repository;
 
-    UsuarioController(UsuarioRepository usuarioRepository) {
-        this.repository = usuarioRepository;
+    @GetMapping
+    public Page<Usuario> usuariosPage(@PageableDefault Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
-    @GetMapping
+    @RequestMapping(value = "/all")
     public List findAll(){
         return repository.findAll();
-    }
-
-    @GetMapping(path = {"/{id}"})
-    public ResponseEntity<Usuario> findById(@PathVariable long id){
-        return repository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
     }
 }
