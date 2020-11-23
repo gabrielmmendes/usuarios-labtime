a830b2ff68fbc6a0cc780a585209681a4e6c1033
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-23 11:44:33 -0300

    Implementação classe UsuarioController

--- src/main/java/br/com/gabrielmmendes/controller/UsuarioController.java
+++ src/main/java/br/com/gabrielmmendes/controller/UsuarioController.java
@@ -1,4 +1,30 @@
 package br.com.gabrielmmendes.controller;
 
+import br.com.gabrielmmendes.model.Usuario;
+import br.com.gabrielmmendes.repository.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/usuarios"})
 public class UsuarioController {
+    private UsuarioRepository repository;
+
+    UsuarioController(UsuarioRepository usuarioRepository) {
+        this.repository = usuarioRepository;
+    }
+
+    @GetMapping
+    public List findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<Usuario> findById(@PathVariable long id){
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
