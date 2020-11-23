8cf92e186ea061821d929a294f33d6b22ca14962
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-23 11:34:43 -0300

    Mudança na formatação da data

--- src/main/java/br/com/gabrielmmendes/model/Usuario.java
+++ src/main/java/br/com/gabrielmmendes/model/Usuario.java
@@ -1,9 +1,12 @@
 package br.com.gabrielmmendes.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
@@ -18,6 +21,7 @@ public class Usuario {
     private Long codigo;
     private String nomeCompleto;
     private String nomeSocial;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
     private Date dataNascimento;
     private String sexo;
     private String email;
@@ -25,5 +29,6 @@ public class Usuario {
     private String municipio;
     private Integer numeroAcessosCurso;
     private String situacaoCurso;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
     private Date dataVinculo;
 }
