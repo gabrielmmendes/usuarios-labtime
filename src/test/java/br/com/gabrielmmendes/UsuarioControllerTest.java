bd5d8be4e33fa379acea4e3e0f4673be87a5857b
Gabriel Mendes <gabrielmendes.0904@gmail.com>
2020-11-25 13:01:32 -0300

    novo metodo para testar o objeto repository do Spring Data

--- src/test/java/br/com/gabrielmmendes/UsuarioControllerTest.java
+++ src/test/java/br/com/gabrielmmendes/UsuarioControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.gabrielmmendes;
 
+import java.util.List;
+
 import br.com.gabrielmmendes.controller.UsuarioController;
+import br.com.gabrielmmendes.model.Usuario;
 import br.com.gabrielmmendes.repository.UsuarioRepository;
 import static br.com.gabrielmmendes.PageableAssert.assertThatPageable;
 
@@ -14,6 +17,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,9 +37,9 @@ public class UsuarioControllerTest {
     @Test //teste de integração que mostra que a página e o tamanho dos parâmetros de consulta agora são avaliados e "injetados" no argumento Pageable do nosso método de controlador da web.
     public void usuariosPageTeste() throws Exception {
         mockMvc.perform(get("/usuarios")
-            .param("page", "0")
-            .param("size", "10"))
-            .andExpect(status().isOk());
+                .param("page", "0")
+                .param("size", "10"))
+                .andExpect(status().isOk());
 
         ArgumentCaptor<Pageable> pageableCaptor = ArgumentCaptor.forClass(Pageable.class);
         verify(repository).findAll(pageableCaptor.capture());
@@ -41,5 +47,11 @@ public class UsuarioControllerTest {
 
         assertThatPageable(pageable).hasPageNumber(0);
         assertThatPageable(pageable).hasPageSize(10);
+    }
+
+    @Test //teste de integração responsável por testar o objeto repository do Spring Data, ele está solicitando o repositório para todos os usuários e, em seguida, usando o Hamcrest para garantir que a lista de retorno seja maior ou igual a 0.
+    public void testFindAll() {
+        List<Usuario> usuario = repository.findAll();
+        assertThat(usuario.size(), is(greaterThanOrEqualTo(0)));
     }
 }
